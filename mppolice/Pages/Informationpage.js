0af2fd2eae181461905b469e7ca631959f444f51
prankurpandeyy <pprankur@gmail.com>
2024-08-14 11:49:11 +0530

    ui/ux

--- mppolice/Pages/Informationpage.js
+++ mppolice/Pages/Informationpage.js
@@ -1,13 +1,22 @@
 import { View, Text, SafeAreaView, ScrollView, StyleSheet } from "react-native";
 import React from "react";
 import Information from "../Components/Information";
+import { Linking } from "react-native";
+
 const Informationpage = ({ navigation }) => {
   return (
     <SafeAreaView>
       <ScrollView style={styles.container}>
         <Information navigation={navigation} />
-        <Text style={styles.devInfo}>
-          Designed & Developed by ATD Softwares Maihar
+        <Text
+          style={styles.devInfo}
+          onPress={() => {
+            Linking.openURL("https://atdsoftwares.netlify.app/");
+          }}
+        >
+          <Text style={{ fontWeight: "bold", color: "#1D9BF0" }}>
+            Designed & Developed by ATD Softwares Maihar
+          </Text>
         </Text>
       </ScrollView>
     </SafeAreaView>
@@ -23,7 +32,6 @@ const styles = StyleSheet.create({
     fontSize: 15,
     textAlign: "center",
     margin: 2,
-   
   },
 });
 
